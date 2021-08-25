59976050dcc03bda68c3bb31953acf00acd20406
saichaitanya18 <70934545+saichaitanya18@users.noreply.github.com>
2021-08-25 09:49:19 +0530

    Create sdp.sql

---
+++ sdp.sql
@@ -0,0 +1,6 @@
+create table supply(ssn int primary key,name varchar(10),countryname varchar(10));
+desc supply;
+insert into supply values(1,'rajesh','india');
+insert into supply values(2,'john','US');
+insert into supply values(3,'zing','uk');
+select * from supply;
